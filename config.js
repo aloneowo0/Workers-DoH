ce29ed0cac189b6366e5d40481d28ac4921c19ba
aloneowo <aloneowo@users.noreply.github.com>
2026-05-27 18:33:22 +0800

    .env 全部上游启用 + 生成 config.js

--- config.js
+++ config.js
@@ -4,11 +4,16 @@
  */
 
 export const UPSTREAMS = {
+    360         : { url: 'https://doh.360.cn/dns-query', ecs: true , plus: true  },
     google      : { url: 'https://dns.google/dns-query', ecs: true , plus: true  },
     cloudflare  : { url: 'https://cloudflare-dns.com/dns-query', ecs: true , plus: true  },
     quad9       : { url: 'https://dns11.quad9.net/dns-query', ecs: true , plus: true  },
     adguard     : { url: 'https://dns.adguard-dns.com/dns-query', ecs: true , plus: true  },
     opendns     : { url: 'https://dns.opendns.com/dns-query', ecs: true , plus: true  },
+    yandex      : { url: 'https://common.dot.dns.yandex.net/dns-query', ecs: false, plus: false },
+    dnspod      : { url: 'https://sm2.doh.pub/dns-query', ecs: true , plus: true  },
+    alidns      : { url: 'https://dns.alidns.com/dns-query', ecs: true , plus: true  },
+    nextdns     : { url: 'https://dns.nextdns.io', ecs: true , plus: true  },
 };
 
 export const ECS_PROTECT_MS = 20;
